1dfd2a6beb12d0fb4b7b0fa364bf4bd60ca787a0
Yogesh <yogesh@graniteriverlabs.in>
2025-01-02 11:09:52 +0530

    Factory Pattern

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -3,7 +3,8 @@
 enum EnumDesignPatterns
 {
     None = 0,
-    Singleton = 1
+    Factory = 1,
+    Singleton = 2
 }
 
 
@@ -11,13 +12,27 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        EnumDesignPatterns designPattern = EnumDesignPatterns.Singleton;
+        string designPatternString = "Enter the value of design pattern\n";
+        foreach (EnumDesignPatterns enumDesignPatterns in Enum.GetValues(typeof(EnumDesignPatterns)))
+        {
+            designPatternString += $"{(int)enumDesignPatterns} - {enumDesignPatterns}\n";
+        }
+        Console.WriteLine(designPatternString);
+
+        int val = Int32.TryParse(Console.ReadLine(), out int parsedVal) ? parsedVal : 0;
+        EnumDesignPatterns designPattern = (EnumDesignPatterns)val;
 
         switch (designPattern)
         {
             case EnumDesignPatterns.Singleton:
                 Singleton singleton = Singleton.GetInstance();
                 singleton.PrintMessage();
+                break;
+            case EnumDesignPatterns.Factory:
+                IProduct product = Factory.GetProduct("ColourPrinter");
+                product.Printer();
+                IProduct product2 = Factory.GetProduct("BlackWhitePrinter");
+                product2.Printer();
                 break;
             default:
                 Console.WriteLine("Invalid Pattern");
